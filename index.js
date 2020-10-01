5c9ef64cac3182d9da405875a118365a174041ff
James Ross <james@jross.me>
2020-10-01 22:18:15 +0100

    feat: publish first version of @nodecraft/browserslist-config

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+	"production": [
+		"> 0.5% in my stats",
+		"last 3 chrome versions",
+		"last 3 edge versions",
+		"last 3 firefox versions",
+		"firefox esr",
+		"last 3 ios versions",
+		"last 3 safari versions",
+		"last 1 samsung versions",
+		"not ie <= 11",
+		"not ChromeAndroid 4",
+		"not dead"
+	],
+	"development": [
+		"last 1 chrome version",
+		"last 1 firefox version",
+		"last 1 edge version"
+	]
+};
