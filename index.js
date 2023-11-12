62e1f3d6866b7aa155390513a6a7ae9d8455e271
James Ross <james@jross.me>
2023-11-12 18:50:47 +0000

    chore: bump dependencies

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 module.exports = {
-	"production": [
-		"last 3 chrome versions",
-		"last 3 edge versions",
-		"last 3 firefox versions",
-		"firefox esr",
-		"last 3 ios versions",
-		"last 3 safari versions",
-		"last 1 samsung versions",
+	'production': [
+		'last 3 chrome versions',
+		'last 3 edge versions',
+		'last 3 firefox versions',
+		'firefox esr',
+		'last 3 ios versions',
+		'last 3 safari versions',
+		'last 1 samsung versions',
 	],
-	"development": [
-		"last 1 chrome version",
-		"last 1 firefox version",
-		"last 1 edge version",
+	'development': [
+		'last 1 chrome version',
+		'last 1 firefox version',
+		'last 1 edge version',
 	],
 };
